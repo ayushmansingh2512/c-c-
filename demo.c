9daccb717af819decba2b7812136bc0c9e207d52
ayushman <ayushmansingh2512@gmail.com>
2025-04-03 22:48:50 +0530

    add my first c file

---
+++ demo.c
@@ -0,0 +1,16 @@
+/*  double quote "" are not sring byut array
+  %d  -> number like 42 ||     %.1f -> floating number like 3.5  || %s -> sarah character need to terminated array which at the 0 at the end
+ 	
+ *  */
+
+#include <stdio.h>
+	int main(){
+		int usf,euf;
+		printf("Enter US Floor \n");
+		scanf("%d", &usf);
+		euf = usf-1;
+		printf("EU Floor %d\n", euf);
+
+
+
+}

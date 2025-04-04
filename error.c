a092b20ed7f338a348243de30327fef9fddc7389
ayushman <ayushmansingh2512@gmail.com>
2025-04-04 13:33:30 +0530

    Added program realted C++

---
+++ error.c
@@ -0,0 +1,14 @@
+//compile time error
+//runtime erro
+//warnings
+//
+//compile time error ->c++(with some problem) -> compiler -> will not run show you the error and tell you remove there  will not compile
+//run time rror -> it will compile it will not goona do what you wanted it do  and sometime is runtime error failed lead program to crash
+//warning --> its nt gooona halt the prgram but tell you what you are doing is problematic 
+//
+#include <iostream>
+
+int main(){
+	std::cout<<"hello world" << std::endl
+		return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>//include library 
+
+int main() { // main fuctiion also called the entery posing  the c++ program and these will run in dorder
+std::cout << "hello C++" << std::endl; // this will run first 
+std::cout <<"number 1" << std::endl;  //<< we need to use this fdor new line std::endl will not as it include this
+std::cout <<"Number 2"<< std::endl;
+std::cout << "Number 3" << std::endl;
+
+return 0;// this will run second
+}
+
+
+
+

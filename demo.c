83a00ea958b4d5d6bf16a865554264ae51f97003
ayushman <ayushmansingh2512@gmail.com>
2025-04-04 13:50:54 +0530

    checking

--- demo.c
+++ demo.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 	int main(){
 		int usf,euf;
-		printf("Enter US Floor \n");
+		printf("Enter US Floor right now \n");
 		scanf("%d", &usf);
 		euf = usf-1;
 		printf("EU Floor %d\n", euf);
